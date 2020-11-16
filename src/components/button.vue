064202be267586888174f3240577d0c872d80f01
kai <b10223052@yuntech.org.tw>
2020-11-16 14:43:05 +0800

    add button component

---
+++ src/components/button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="$emit('on-click')">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  width: 350px;
+  height: 58px;
+  background-color: $black;
+  font-size: 36px;
+  color: $orange;
+  text-align: center;
+  border: none;
+  cursor: pointer;
+}
+</style>
